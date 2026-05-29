6fa5460860b5fe3460ded593982dd74842160e0c
Lagerq2 <eliaslagerqvist@outlook.com>
2026-05-29 13:38:18 +0200

    Restore backend git configuration

---
+++ src/main/java/systementor/cloudstoreuserservice/config/SecurityConfig.java
@@ -0,0 +1,113 @@
+package systementor.cloudstoreuserservice.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import systementor.cloudstoreuserservice.repository.user.AppUserRepository;
+import systementor.cloudstoreuserservice.security.JwtAuthenticationFilter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products").permitAll()
+
+                        /** TODO FIX ISSUE WITH ORDERS, IT SAYS NOT AUTHENTICATED ATM*/
+                        .requestMatchers("/api/orders","/api/orders/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(AppUserRepository userRepository) {
+        return email -> userRepository.findByEmail(email)
+                .map(appUser -> User
+                        .withUsername(appUser.getEmail())
+                        .password(appUser.getPassword())
+                        .roles(appUser.getRole())
+                        .build()
+                )
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowedOrigins(List.of(
+                    "http://localhost:5000",
+                    "http://localhost:8081",
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5500"
+                    ));
+
+            config.setAllowedMethods(List.of(
+                    "GET",
+                    "POST",
+                    "PUT",
+                    "DELETE",
+                    "OPTIONS"
+            ));
+            config.setAllowedHeaders(List.of("*"));
+
+            config.setAllowCredentials(true);
+
+            return config;
+        };
+    }
+}
