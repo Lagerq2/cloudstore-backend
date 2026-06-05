77fe81ac390fa639a1da6009bf6643f28af2fa20
Lagerq2 <eliaslagerqvist@outlook.com>
2026-06-05 02:38:35 +0200

    try/fix add httpS to Yaya fake store API

--- src/main/java/systementor/cloudstoreuserservice/product/client/ProductClient.java
+++ src/main/java/systementor/cloudstoreuserservice/product/client/ProductClient.java
@@ -13,7 +13,7 @@ public class ProductClient {
 
     private final RestClient restClient;
     //private final String fakestoreServiceUrl = "http://fakestoreservice-env.eba-qva25xqa.eu-north-1.elasticbeanstalk.com/products";
-    private final String fakestoreServiceUrl = "http://yahyatesting-env.eba-sarnymwd.eu-north-1.elasticbeanstalk.com/products";
+    private final String fakestoreServiceUrl = "https://yahyatesting-env.eba-sarnymwd.eu-north-1.elasticbeanstalk.com/products";
 
     public ProductClient(RestClient.Builder builder) {
         this.restClient = builder
